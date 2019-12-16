4cbc31a215816b390f1f811c0280b30c7aaa0d29
Avinaba-Ray <avinabaray@gmail.com>
2019-12-17 00:25:35 +0530

    activity_seller_logged_in.xml modified with all fields. Firebase Storage dependency added.

--- app/src/main/java/com/avinabaray/oldsold/SellerLoggedIn.java
+++ app/src/main/java/com/avinabaray/oldsold/SellerLoggedIn.java
@@ -7,20 +7,42 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SellerLoggedIn extends BaseActivity {
 
-        @Override
-        public void onBackPressed() {
-            // Uncomment the line below to enable BACK button
+    @Override
+    public void onBackPressed() {
+        // Uncomment the line below to enable BACK button
 //        super.onBackPressed();
-        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seller_logged_in);
+
+        // Setup Dropdown Menu
+        List<String> itemCategories = new ArrayList<String>();
+        itemCategories.add("Mobile Phones");
+        itemCategories.add("Furniture");
+        itemCategories.add("Home Appliances");
+        itemCategories.add("Gadgets");
+
+        Spinner spinnerItemCategory = findViewById(R.id.spinnerItemCategory);
+        ArrayAdapter<String> itemCategoryAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_spinner_dropdown_item,
+                itemCategories
+        );
+        spinnerItemCategory.setAdapter(itemCategoryAdapter);
+
+
     }
 }
