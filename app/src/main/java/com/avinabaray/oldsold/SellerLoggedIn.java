9210e6e854bfd22a1b72107fbd5a36d4208b205a
Avinaba-Ray <avinabaray@gmail.com>
2019-12-18 02:48:22 +0530

    Seller's item image uploaded to Firebase Storage. Item name stored in database.

--- app/src/main/java/com/avinabaray/oldsold/SellerLoggedIn.java
+++ app/src/main/java/com/avinabaray/oldsold/SellerLoggedIn.java
@@ -2,27 +2,40 @@ package com.avinabaray.oldsold;
 
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 public class SellerLoggedIn extends BaseActivity implements AdapterView.OnItemSelectedListener {
 
@@ -30,14 +43,21 @@ public class SellerLoggedIn extends BaseActivity implements AdapterView.OnItemSe
     public static final String I_TITLE = "i_title";
     public static final String I_DESC = "i_desc";
     public static final String SELLER_ID = "seller_id";
+    public static final String I_IMAGE = "i_image";
     private EditText editTextItemTitle;
     private EditText editTextItemDesc;
+    private ImageView imageViewSelectedImg;
     private String itemTitle;
     private String itemDesc;
+    private String itemPhotoName;
     private String itemCatg = "Mobile Phones";
+    private Uri filePath;
+    private final int PICK_IMAGE_REQUEST = 71;
 
     private AlertDialog.Builder alertBuilder;
     private CollectionReference mDocRef = FirebaseFirestore.getInstance().collection("items");
+    FirebaseStorage firebaseStorage;
+    StorageReference storageReference;
     CommonMethods commonMethods = new CommonMethods();
 
     @Override
@@ -52,10 +72,14 @@ public class SellerLoggedIn extends BaseActivity implements AdapterView.OnItemSe
         setContentView(R.layout.activity_seller_logged_in);
         alertBuilder = new AlertDialog.Builder(SellerLoggedIn.this);
 
-        // Edit Text Data
+        // Initialize Storage objects
+        firebaseStorage = FirebaseStorage.getInstance();
+        storageReference = firebaseStorage.getReference();
+
+        // Initialize Views
         editTextItemTitle = findViewById(R.id.editTextItemTitle);
         editTextItemDesc = findViewById(R.id.editTextItemDesc);
-
+        imageViewSelectedImg = findViewById(R.id.imageViewSelectedImg);
 
         // Setup Dropdown Menu
         List<String> itemCategories = new ArrayList<String>();
@@ -76,28 +100,92 @@ public class SellerLoggedIn extends BaseActivity implements AdapterView.OnItemSe
 
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null )
+        {
+            filePath = data.getData();
+            Log.wtf("File name", filePath.toString());
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                imageViewSelectedImg.setImageBitmap(bitmap);
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void selectPhoto(View view) {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+
+    }
+
     public void uploadItem(View view) {
         itemTitle = editTextItemTitle.getText().toString();
         itemDesc = editTextItemDesc.getText().toString();
 
-        // Data to be pushed to Firestore
-        Map<String, Object> itemDetails = new HashMap<String, Object>();
-        itemDetails.put(SELLER_ID, MainActivity.CURRENT_USER_ID);
-        itemDetails.put(I_CATEGORY, itemCatg);
-        itemDetails.put(I_TITLE, itemTitle);
-        itemDetails.put(I_DESC, itemDesc);
+        itemPhotoName = MainActivity.CURRENT_USER_ID + "_" + UUID.randomUUID().toString();
 
-        mDocRef.add(itemDetails).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentReference> task) {
-                if (task.isSuccessful()) {
-                    commonMethods.createAlert(alertBuilder, "Item uploaded successfully!");
-                } else {
-                    String errorMsg = task.getException().getMessage();
-                    commonMethods.createAlert(alertBuilder, errorMsg);
+        // Photo to be uploaded to Firebase Storage
+        if(filePath != null)
+        {
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Uploading...");
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+
+            StorageReference ref = storageReference.child("item_images/"+ itemPhotoName);
+            ref.putFile(filePath).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                    filePath = null;
+                    imageViewSelectedImg.setImageResource(0);
+
+                    // Data to be pushed to Firestore
+                    Map<String, Object> itemDetails = new HashMap<String, Object>();
+                    itemDetails.put(SELLER_ID, MainActivity.CURRENT_USER_ID);
+                    itemDetails.put(I_CATEGORY, itemCatg);
+                    itemDetails.put(I_TITLE, itemTitle);
+                    itemDetails.put(I_IMAGE, itemPhotoName);
+                    itemDetails.put(I_DESC, itemDesc);
+
+                    mDocRef.add(itemDetails).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentReference> task) {
+                            progressDialog.dismiss();
+                            if (task.isSuccessful()) {
+                                commonMethods.createAlert(alertBuilder, "Item uploaded successfully!");
+                            } else {
+                                String errorMsg = task.getException().getMessage() + " Try Again...";
+                                commonMethods.createAlert(alertBuilder, errorMsg);
+                            }
+                        }
+                    });
                 }
-            }
-        });
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    progressDialog.dismiss();
+                    commonMethods.createAlert(alertBuilder, "Failed " + e.getMessage());
+                }
+            }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onProgress(@NonNull UploadTask.TaskSnapshot taskSnapshot) {
+                    double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
+                    progressDialog.setMessage("Uploaded " + (int)progress + "%");
+                }
+            });
+        } else {
+            commonMethods.createAlert(alertBuilder, "Please select a photo to upload the item");
+        }
     }
 
     @Override
